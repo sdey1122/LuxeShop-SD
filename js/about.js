55a5e6bac8db8507cf5a442e2bac098df86b6656
Subhankar Dey <sdey1122@gmail.com>
2025-11-09 00:55:22 +0530

    new_page_about_us_initial_commit_navbar_responsive

---
+++ js/about.js
@@ -0,0 +1,52 @@
+// Search Input
+const searchInput = document.querySelector(".search-input");
+const searchWrap = document.querySelector(".search-wrap");
+
+searchInput.addEventListener("focus", () => {
+  searchWrap.classList.add("active");
+});
+searchInput.addEventListener("blur", () => {
+  if (searchInput.value.trim() === "") {
+    searchWrap.classList.remove("active");
+  }
+});
+
+// Heart Icon Toggle Color
+const heartIcon = document.getElementById("heartIcon");
+
+heartIcon.addEventListener("click", () => {
+  if (heartIcon.classList.contains("fa-regular")) {
+    heartIcon.classList.remove("fa-regular");
+    heartIcon.classList.add("fa-solid", "liked");
+  } else {
+    heartIcon.classList.remove("fa-solid", "liked");
+    heartIcon.classList.add("fa-regular");
+  }
+});
+
+// Heart Icon Toggle Color (Product Page Card)
+const podHeart = document.getElementById("pod-heart");
+
+podHeart.addEventListener("click", () => {
+  if (podHeart.classList.contains("fa-regular")) {
+    podHeart.classList.remove("fa-regular");
+    podHeart.classList.add("fa-solid", "liked");
+  } else {
+    podHeart.classList.remove("fa-solid", "liked");
+    podHeart.classList.add("fa-regular");
+  }
+});
+
+// Active Page
+const currentPage = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".hover-link");
+
+navLinks.forEach((link) => {
+  const linkPage = link.getAttribute("href").split("/").pop();
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
+  }
+});
