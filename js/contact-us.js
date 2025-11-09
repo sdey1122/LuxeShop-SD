b402822b8414706329c6ec368949493aa069ff28
Subhankar Dey <sdey1122@gmail.com>
2025-11-10 04:47:22 +0530

    contact_us_header_banner_responsive

---
+++ js/contact-us.js
@@ -0,0 +1,39 @@
+// Search Input
+const searchInput = document.querySelector(".search-input");
+const searchWrap = document.querySelector(".search-wrap");
+
+searchInput.addEventListener("focus", () => {
+  searchWrap.classList.add("active");
+});
+searchInput.addEventListener("blur", () => {
+  if (searchInput.value.trim() === "") {
+    searchWrap.classList.remove("active");
+  }
+});
+
+// Heart Icon Toggle Color
+const heartIcon = document.getElementById("heartIcon");
+
+heartIcon.addEventListener("click", () => {
+  if (heartIcon.classList.contains("fa-regular")) {
+    heartIcon.classList.remove("fa-regular");
+    heartIcon.classList.add("fa-solid", "liked");
+  } else {
+    heartIcon.classList.remove("fa-solid", "liked");
+    heartIcon.classList.add("fa-regular");
+  }
+});
+
+// Active Page
+const currentPage = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".hover-link");
+
+navLinks.forEach((link) => {
+  const linkPage = link.getAttribute("href").split("/").pop();
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
+  }
+});
